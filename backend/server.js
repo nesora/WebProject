93cc470b0b440e100830e780152ac325fd4e5f59
nesora <rosensoul@gmail.com>
2022-01-16 18:36:18 +0200

    Added MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,26 @@
 import express from 'express';
 import data from './data';
+import config from './config';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose'
+import userRoute from './routes/userRoute';
+
+dotenv.config();
+
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true, // must be added always, because required
+});
+
+mongoose.connection
+  .once('open', () => console.log('Connected'))
+  .on('error', (error) => {
+    console.log('Your Error', error);
+  });
 
 const app = express();
+
+app.use('/api/users', userRoute);
 
 app.get("/api/products/:id", (req, res) => {
   const productId = req.params.id;
